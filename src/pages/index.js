dfc0bda60d0ed5eb7e525f78045b118341989508
Allan Ramos <allangrds@gmail.com>
2020-12-09 01:17:23 -0300

    fix(package.json): remove ^ of dependencies

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,6 @@ import styles from './style.module.css'
 const IndexPage = () => {
   return (
     <main className={styles.pageStyles}>
-      <title>Home Page</title>
       <h1>asdadas</h1>
     </main>
   )
